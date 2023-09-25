5662f56fb2309c9a71b923dd026a6439392aca37
M1Al3x <90932420+M1Al3x@users.noreply.github.com>
2023-09-25 17:43:56 -0600

    Update proxy_client

--- proxy_client.py
+++ proxy_client.py
@@ -1,39 +1,19 @@
-import socket
-from threading import Thread
+import socket 
 
 BYTES_TO_READ = 4096
-HOST = "127.0.0.1"
-PORT = 8001
 
-def handle_connection(conn, addr):
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(BYTES_TO_READ)
-            if not data:
-                break
-            print(data)
-            conn.sendall(data)
-
-def start_server():
+def get(host, port):
+    request = b"GET / HTTP/1.1\nHOST: www.google.com\n\n"
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.listen()
-        conn, addr = s.accept()
-        handle_connection(conn, addr)
+        s.connect((host, port))
+        s.send(request)
+        s.shutdown(socket.SHUT_WR)
+        chunk = s.recv(BYTES_TO_READ)
+        result = b'' + chunk 
+        while(len(chunk)>0):
+            chunk = s.recv(BYTES_TO_READ)
+            result += chunk
+        s.close()
+        return result 
 
-def start_theard_server():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.listen(2)
-        while True:
-            conn, addr = s.accept()
-            print("Connected by", addr)
-            thread = Thread(target=handle_connection, args=(conn, addr))
-            thread.run()
-
-
-if __name__ == "__main__":
-    start_theard_server()
+print(get("127.0.0.1", 8000))
